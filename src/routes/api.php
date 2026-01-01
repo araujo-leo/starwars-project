ebff88beabc4bd514cc7abe7ffe06f4dba18fed2
leonardo araujo <leoaraujofnd@gmail.com>
2026-01-01 19:17:25 -0300

    feat: add API routes for all resources

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,11 +2,16 @@
 
 use App\Core\Router;
 use App\Controller\FilmController;
-
+use App\Controller\CharacterController;
 
 $router = new Router();
 
 $router->get('/api/films', [FilmController::class, 'listFilms']);
 $router->get('/api/films/{id}', [FilmController::class, 'getFilmsById']);
 
+$router->get('/api/characters/{id}', [CharacterController::class, 'getCharacter']);
+$router->get('/api/planets/{id}', [PlanetController::class, 'getPlanet']);
+$router->get('/api/starships/{id}', [StarshipController::class, 'getStarship']);
+$router->get('/api/vehicles/{id}', [VehicleController::class, 'getVehicle']);
+$router->get('/api/species/{id}', [SpeciesController::class, 'getSpecies']);
 return $router;
