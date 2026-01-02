303f38b21f95d4250201c4cb0c32bf75cc8c70a1
leonardo araujo <leoaraujofnd@gmail.com>
2026-01-02 20:49:02 -0300

    feat: implement logging for planet fetching operations in PlanetController; log successful and error responses

--- src/Controller/PlanetController.php
+++ src/Controller/PlanetController.php
@@ -2,22 +2,28 @@
 
 namespace App\Controller;
 
+use App\Model\Log;
 use App\Service\SwapiService;
 
 class PlanetController
 {
     public function listPlanets() {
         try {
+            $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
             $swapiService = new SwapiService();
             $planets = $swapiService->fetchAllPlanets();
             header('Content-Type: application/json');
+
+            Log::save('INFO', "/films?page=$page");
             echo json_encode([
                 'succes' => true,
                 'data' => $planets
             ], 200);
-        } catch (Exception $e) {
+        } catch (\Exception $e) {
             http_response_code(500);
             error_log($e->getMessage());
+
+            Log::save('ERROR', "/planets" );
             echo json_encode([
                 'error' => true,
                 'message' => 'Failed to fetch planets'
@@ -30,12 +36,17 @@ class PlanetController
             $swapiService = new SwapiService();
             $planets = $swapiService->fetchPlanetById($id);
             header('Content-Type: application/json');
+
+            Log::save('INFO', "/planets/$id");
             echo json_encode([
                 'success' => true,
                 'data' => $planets
             ], 200);
         } catch (\Exception $e) {
+            http_response_code(500);
             error_log($e->getMessage());
+
+            Log::save('ERROR', "/planets");
             echo json_encode([
                 'error' => true,
                 'message' => 'Failed to fetch planets'
