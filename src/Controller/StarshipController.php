1e82a681d945a73877a1077d0dd164e5e77dafa0
leonardo araujo <leoaraujofnd@gmail.com>
2026-01-02 20:53:40 -0300

    feat: implement logging for starship fetching operations in StarshipController; log successful and error responses

--- src/Controller/StarshipController.php
+++ src/Controller/StarshipController.php
@@ -2,22 +2,28 @@
 
 namespace App\Controller;
 
+use App\Model\Log;
 use App\Service\SwapiService;
 
 class StarshipController
 {
     public function listStarships() {
         try {
+            $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
             $swapiService = new SwapiService();
             $starships = $swapiService->fetchAllStarships();
             header('Content-Type: application/json');
+
+            Log::save('INFO', "/starships?page=$page");
             echo json_encode([
                 'succes' => true,
                 'data' => $starships
             ], 200);
-        } catch (Exception $e) {
+        } catch (\Exception $e) {
             http_response_code(500);
             error_log($e->getMessage());
+
+            Log::save('ERROR', "/starships?page=$page");
             echo json_encode([
                 'error' => true,
                 'message' => 'Failed to fetch starships'
@@ -30,12 +36,17 @@ class StarshipController
             $swapiService = new SwapiService();
             $starships = $swapiService->fetchStarshipById($id);
             header('Content-Type: application/json');
+
+            Log::save('INFO', "/starships/$id");
             echo json_encode([
                 'success' => true,
                 'data' => $starships
             ], 200);
         } catch (\Exception $e) {
+            http_response_code(500);
             error_log($e->getMessage());
+
+            Log::save('ERROR', "/starships/$id");
             echo json_encode([
                 'error' => true,
                 'message' => 'Failed to fetch starships'
