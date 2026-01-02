dd845d43b8ba7851006c560b77935d4aa6ccd165
leonardo araujo <leoaraujofnd@gmail.com>
2026-01-02 20:53:44 -0300

    feat: implement logging for vehicle fetching operations in VehicleController; log successful and error responses

--- src/Controller/VehicleController.php
+++ src/Controller/VehicleController.php
@@ -2,22 +2,28 @@
 
 namespace App\Controller;
 
+use App\Model\Log;
 use App\Service\SwapiService;
 
 class VehicleController
 {
     public function listVehicles() {
         try {
+            $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
             $swapiService = new SwapiService();
             $vehicles = $swapiService->fetchAllVehicles();
             header('Content-Type: application/json');
+
+            Log::save('INFO', "/vehicles?page=$page");
             echo json_encode([
                 'succes' => true,
                 'data' => $vehicles
             ], 200);
-        } catch (Exception $e) {
+        } catch (\Exception $e) {
             http_response_code(500);
             error_log($e->getMessage());
+
+            Log::save('ERROR', "/vehicles?page=$page");
             echo json_encode([
                 'error' => true,
                 'message' => 'Failed to fetch vehicles'
@@ -30,12 +36,17 @@ class VehicleController
             $swapiService = new SwapiService();
             $vehicles = $swapiService->fetchVehicleById($id);
             header('Content-Type: application/json');
+
+            Log::save('INFO', "/vehicles/$id");
             echo json_encode([
                 'success' => true,
                 'data' => $vehicles
             ], 200);
         } catch (\Exception $e) {
+            http_response_code(500);
             error_log($e->getMessage());
+
+            Log::save('ERROR', "/vehicles/$id");
             echo json_encode([
                 'error' => true,
                 'message' => 'Failed to fetch vehicles'
