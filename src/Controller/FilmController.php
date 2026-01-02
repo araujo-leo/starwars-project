407ecee0775bcfcbc7c450440ff5db1f4bac34f6
leonardo araujo <leoaraujofnd@gmail.com>
2026-01-02 20:44:19 -0300

    feat: implement logging for film fetching operations in FilmController; log successful and error responses

--- src/Controller/FilmController.php
+++ src/Controller/FilmController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 use App\Core\View;
+use App\Model\Log;
 use App\Service\SwapiService;
 use App\Config\Database;
 use Exception;
@@ -20,6 +21,8 @@ class FilmController {
             $swapiService = new SwapiService();
             $films = $swapiService->fetchAllFilms();
             header('Content-Type: application/json');
+
+            Log::save('INFO', "/films");
             echo json_encode([
                 'succes' => true,
                 'data' => $films
@@ -27,6 +30,8 @@ class FilmController {
         } catch (Exception $e) {
             http_response_code(500);
             error_log($e->getMessage());
+
+            Log::save('ERROR', "/films");
             echo json_encode([
                 'error' => true,
                 'message' => 'Failed to fetch films'
@@ -39,12 +44,17 @@ class FilmController {
             $swapiService = new SwapiService();
             $films = $swapiService->fetchFilmById($id);
             header('Content-Type: application/json');
+
+            Log::save('INFO', "/films/$id");
             echo json_encode([
                 'succes' => true,
                 'data' => $films
             ], 200);
         } catch (Exception $e) {
+            http_response_code(500);
             error_log($e->getMessage());
+
+            Log::save('ERROR', "/films/$id");
             echo json_encode([
                 'error' => true,
                 'message' => 'Failed to fetch films'
