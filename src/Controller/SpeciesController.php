0fd27a287858e6e29f477c915a7bc8aa405b08ef
leonardo araujo <leoaraujofnd@gmail.com>
2026-01-02 20:53:32 -0300

    feat: implement logging for species fetching operations in SpeciesController; log successful and error responses

--- src/Controller/SpeciesController.php
+++ src/Controller/SpeciesController.php
@@ -2,15 +2,19 @@
 
 namespace App\Controller;
 
+use App\Model\Log;
 use App\Service\SwapiService;
 
 class SpeciesController
 {
     public function listSpecies() {
         try {
+            $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
             $swapiService = new SwapiService();
             $species = $swapiService->fetchAllSpecies();
             header('Content-Type: application/json');
+
+            Log::save('INFO', "/species?page=$page");
             echo json_encode([
                 'succes' => true,
                 'data' => $species
@@ -18,6 +22,8 @@ class SpeciesController
         } catch (Exception $e) {
             http_response_code(500);
             error_log($e->getMessage());
+
+            Log::save('ERROR', "/species?page=$page");
             echo json_encode([
                 'error' => true,
                 'message' => 'Failed to fetch species'
@@ -30,12 +36,17 @@ class SpeciesController
             $swapiService = new SwapiService();
             $species = $swapiService->fetchSpecieById($id);
             header('Content-Type: application/json');
+
+            Log::save('INFO', "/species?id=$id");
             echo json_encode([
                 'success' => true,
                 'data' => $species
             ], 200);
         } catch (\Exception $e) {
+            http_response_code(500);
             error_log($e->getMessage());
+
+            Log::save('ERROR', "/species?id=$id");
             echo json_encode([
                 'error' => true,
                 'message' => 'Failed to fetch species'
